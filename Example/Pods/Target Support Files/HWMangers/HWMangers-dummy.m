5ba667f86d1973d196760e35db4ecb149fc44abb
sunbinhua <571100944@qq.com>
2018-12-21 11:45:14 +0800

    创建仓库

---
+++ Example/Pods/Target Support Files/HWMangers/HWMangers-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HWMangers : NSObject
+@end
+@implementation PodsDummy_HWMangers
+@end
---
+++ Example/Pods/Target Support Files/Pods-HWMangers_Tests/Pods-HWMangers_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HWMangers_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_HWMangers_Tests
+@end
